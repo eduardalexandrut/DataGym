1e783c415381c7ff7fe7f534b3d737ccc70bf2ec
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-29 15:39:34 +0200

    Modified ProfileView.

--- src/main/java/application/controller/ProfileController.java
+++ src/main/java/application/controller/ProfileController.java
@@ -3,8 +3,12 @@ package application.controller;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.ChoiceBox;
+
+import java.util.List;
 
 public class ProfileController extends MainController {
+    static final List<String> MEASURE_UNITS = List.of("Kg", "Lbs");
     //Profile fx elements.
    /* @FXML
     private LineChart<String, String> LineChart;*/
@@ -12,4 +16,12 @@ public class ProfileController extends MainController {
     private CategoryAxis x;
     @FXML
     private NumberAxis y;
+    @FXML
+    private ChoiceBox<String> measuresBox;
+
+    @FXML
+    void initialize() {
+        this.measuresBox.getItems().setAll(MEASURE_UNITS);
+        this.measuresBox.setValue(MEASURE_UNITS.stream().filter(e -> e.equals("Kg")).findAny().get());
+    }
 }
