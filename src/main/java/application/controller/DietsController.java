95832f1ae830f227a2d650415dd30e32f3d91c47
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-07-05 00:15:20 +0200

    implemented dietsTable and init method in DietsController.

--- src/main/java/application/controller/DietsController.java
+++ src/main/java/application/controller/DietsController.java
@@ -1,5 +1,33 @@
 package application.controller;
 
-public class DietsController extends Controller {
+import entity.Diete;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+public class DietsController extends Controller {
+    @FXML
+    private TableView dietsTable;
+    @FXML
+    private TableColumn nameCol;
+    @FXML
+    private TableColumn createdCol;
+    @FXML
+    private TableColumn recipeeCol;
+
+    @FXML
+    void initialize() {
+        initDietsTable();
+    }
+
+    private void initDietsTable() {
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("nomeDieta"));
+        //createdCol.setCellValueFactory(new PropertyValueFactory<>("dataCreazione"));
+        //recipeeCol.setCellValueFactory(new PropertyValueFactory<>("ricetta"));
+        ObservableList<Diete> diets = FXCollections.observableArrayList(getQueryManager().getDiete("EduardT"));
+        dietsTable.setItems(diets);
+    }
 }
